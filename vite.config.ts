ab0cf21f2f340f317fd6f268e5786fb43c5ad7cf
idrislaode <lmidris19@gmail.com>
2026-01-05 21:15:32 +0700

    benarin base

--- vite.config.ts
+++ vite.config.ts
@@ -5,10 +5,11 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: "/pytype/",
   server: {
     host: "::",
     port: 8080,
-    base: "/pytype",
+    
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
   resolve: {
